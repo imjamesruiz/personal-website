15e93d908a229ec184fdde61832497fd7dbc302c
imjamesruiz <jamesruiz610@gmail.com>
2025-08-08 02:39:34 -0700

    fix hover effects to be smoother, added backend for email and resume buttons, aligned footer

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("resume-button").addEventListener("click", function () {
+    window.open(
+        "https://drive.google.com/file/d/1KsJ76vANVXz-O0h36m2sCcVvfvn_qlHi/view?usp=sharing",
+        "_blank"
+    );
+});
